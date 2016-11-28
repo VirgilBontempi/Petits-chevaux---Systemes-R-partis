1337fb4c0eca029f7cb9cd01a703d9d4effd1b57
VirgilBontempi <virgilbontempi@hotmail.fr>
2016-11-28 17:19:57 +0100

    Add files via upload

--- Serveur.c
+++ Serveur.c
@@ -23,9 +23,61 @@
 
 
 #include <stdio.h>
+#include <ctype.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include "Serveur.h"
 
 int main(int argc, char **argv)
 {
+	/* ----------------------------
+	   Initialisation des variables
+	   ----------------------------*/
+	// Paramètres du programme
+	int port, nbJoueursPartie, nbChevaux;
+	// Nombre de joueurs connectés
+	int nbJoueursConnectes;
+	// Tableau de structure des processus, taille = nbJoueursPartie
+	char tableauPipe[nbJoueursPartie];
+	
+	port = atoi(argv[1]);
+	nbJoueursPartie = atoi(argv[2]);
+	nbChevaux = atoi(argv[3]);
+	
+	nbJoueursConnectes = 0;
+	
+	num = socketServer(port, TCP);
+	
+	// Saisie controlée nb joueurs de la partie
+	if(nbJoueursPartie > 4 || nbJoueursPartie < 2)
+	{
+		printf("Une partie doit contenir entre 2 et 4 joueurs.");
+		exit(0);
+	}
+	// Saisie controlée nb chevaux de la partie
+	if(nbChevaux > 4 || nbChevaux < 2)
+	{
+		printf("Un joueur ne peut posséder que 2 à 4 chevaux dans son écurie.");
+		exit(0);
+	}
+	
+	/* -----------------
+	   Le serveur écoute
+	   -----------------*/
+	int indice;
+	// Tant que tous les joueurs ne sont pas connectés
+	for(indice = 0; indice < nbJoueursConnectes; indice++)
+	{
+		id = fork();
+		if(id == 0) // Fils
+		{
+			int msgSock;
+			msgSock = accept(num, NULL, NULL);
+		}
+		
+	}
+	
+	
 	
 	return 0;
 }
---
+++ Serveur.h
@@ -0,0 +1,6 @@
+#define TAILLE_COULEUR 6
+
+
+typdef struct {
+	int pipeIn[2], pipeOut[2];
+	char couleur[TAILLE_COULEUR]} structClientProcess
