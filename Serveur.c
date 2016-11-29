12dfc47b6d053b3e4ed57c8f2f8432ebc1e6589c
matheyt <matheyt@raypic12>
2016-11-29 17:41:39 +0100

    update Serveur attention com defectueuse

--- Serveur.c
+++ Serveur.c
@@ -35,10 +35,10 @@ int main(int argc, char **argv) {
 
 
     // Saisie controlée nb joueurs de la partie
-   /* if (nbJoueursPartie > 4 || nbJoueursPartie < 2) {
+    if (nbJoueursPartie > 4 || nbJoueursPartie < 2) {
         printf("Une partie doit contenir entre 2 et 4 joueurs.\n");
         exit(0);
-    }*/
+    }
     // Saisie controlée nb chevaux de la partie
     if (nbChevaux > 4 || nbChevaux < 2) {
         printf("Un joueur ne peut posséder que 2 à 4 chevaux dans son écurie.\n");
@@ -86,7 +86,6 @@ int main(int argc, char **argv) {
         if (id == 0) // Fils
         {
             ComProcess(tableauPipe, indice);
-            printf("%d\n",indice);
 
         }
         close(tableauPipe[indice].pipeIn[0]);
@@ -94,7 +93,6 @@ int main(int argc, char **argv) {
         write(tableauPipe[indice].pipeIn[1], "entre un truc\n\0", 15);
         taille = read(tableauPipe[indice].pipeOut[0], reponse, 50);
         printf("%s\n", reponse);
-        printf("%d\n",indice);
     }
 
     return 0;
@@ -103,18 +101,18 @@ int main(int argc, char **argv) {
 /* --------------------------
  * Processus de communication
  * --------------------------*/
-void communicationProcessInit(int numSocket, int commSock, int nbJoueursPartie, bool* tableauJoueurs, structComCliServ* tab) {
+void communicationProcessInit(int numSocket, int index, int nbJoueursPartie, bool* tableauJoueurs, structComCliServ* tab) {
 
     // Etablissement de la connexion
     int msgSock;
     msgSock = accept(numSocket, NULL, NULL);
     // Actualisation du nombre de joueurs connectés
-    tableauJoueurs[commSock] = true;
-    tab->numSock = msgSock;
+    tableauJoueurs[index] = true;
+    tab[index].numSock = msgSock;
 
     // Message d'information: infos sur joueur
     char msg[50];
-    sprintf(msg, "Connexion établie.\nVous êtes le joueur: %d\n", commSock + 1);
+    sprintf(msg, "Connexion établie.\nVous êtes le joueur: %d\n", index + 1);
     write(msgSock, msg, strlen(msg));
 
     // Message d'information: places restantes
@@ -161,9 +159,7 @@ void ComProcess(structComCliServ* tab, int indice) {
 
     close(tab[indice].pipeIn[1]);
     close(tab[indice].pipeOut[0]);
-    printf("a\n");
     taille = read(tab[indice].pipeIn[0], msg, TAILLE_MAX);
-    printf("b\n");
     write(tab[indice].numSock, msg, taille+1);
     taille = read(tab[indice].numSock, msg, TAILLE_MAX);
     write(tab[indice].pipeOut[1], msg, taille);
